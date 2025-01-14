befc994eeb78463abbf87134d84e38900059c75b
KsuyS <solovevak105@gmail.com>
2025-01-15 00:56:50 +0300

    Поправила схему для валидации после внесения изменений.

--- src/utils/PresentationSchema.ts
+++ src/utils/PresentationSchema.ts
@@ -34,9 +34,54 @@ const presentationSchema = {
                                             },
                                             required: ["width", "height"]
                                         },
-                                        type: { enum: ["text", "image"] },
+                                        type: { const: "text" },
+                                        value: { type: "string" },
+                                        fontFamily: { type: "string" },
+                                        fontSize: { type: "number" },
+                                        fontColor: { type: "string" },
+                                        textAlign: { enum: ["left", "center", "right"] },
+                                        fontWeight: { enum: ["normal", "bold"] },
+                                        fontStyle: { enum: ["normal", "italic"] },
+                                        textDecoration: { enum: ["none", "underline"] },
+                                        textCase: { enum: ["none", "capitalize", "uppercase", "lowercase"] }
                                     },
-                                    required: ["id", "position", "size", "type"],
+                                    required: ["id", "position", "size", "type", "value", "fontFamily", "fontSize", "fontColor", "textAlign", "fontWeight", "fontStyle", "textDecoration", "textCase"]
+                                },
+                                {
+                                    type: "object",
+                                    properties: {
+                                        id: { type: "string" },
+                                        position: {
+                                            type: "object",
+                                            properties: {
+                                                x: { type: "number" },
+                                                y: { type: "number" }
+                                            },
+                                            required: ["x", "y"]
+                                        },
+                                        size: {
+                                            type: "object",
+                                            properties: {
+                                                width: { type: "number" },
+                                                height: { type: "number" }
+                                            },
+                                            required: ["width", "height"]
+                                        },
+                                        type: { const: "image" },
+                                        src: { type: "string" },
+                                        borderStyle: {
+                                            enum: [
+                                                "none",
+                                                "black-thick",
+                                                "black-thin",
+                                                "white-thick",
+                                                "white-thin",
+                                                "rounded-oval",
+                                                "rounded-rect"
+                                            ]
+                                        }
+                                    },
+                                    required: ["id", "position", "size", "type", "src"]
                                 }
                             ]
                         }
@@ -63,7 +108,7 @@ const presentationSchema = {
                                 type: "object",
                                 properties: {
                                     type: { const: 'gradient' },
-                                    src: { type: 'string' }
+                                    gradient: { type: 'string' }
                                 },
                                 required: ['type', 'gradient']
                             }
